fe24b641191d12bf6131132de8eb6f4475ef653b
Oghlan Uyghur KASHGARIA <55028222+OGKashgaria@users.noreply.github.com>
2020-03-18 22:28:13 -0500

    Update FindUnique.java

--- src/com/Interview/InterviewTasksMuhtar/String/FindUnique.java
+++ src/com/Interview/InterviewTasksMuhtar/String/FindUnique.java
@@ -16,13 +16,9 @@ public class FindUnique {
     //Solution 1:
 
     public static String unique(String str) {
-
         String[ ] arr=str.split("");
-
         String unique1="";
-
         for(int j=0; j<arr.length; j++) {
-
             int num=0;
 
             for(int i=0; i<arr.length; i++ ) {
