83b654ac0eb7a4436fc9373fb3cb4ac451313fa8
OGKashgaria <oghlan.ghulam.kashgaria@gmail.com>
2020-07-09 07:36:08 -0500

    CountDigits_Of_A_Number and PalindromeNumber
    classes were updated

--- src/com/Interview/InterviewTasksMuhtar/Numbers/CountDigits_Of_A_Number.java
+++ src/com/Interview/InterviewTasksMuhtar/Numbers/CountDigits_Of_A_Number.java
@@ -2,7 +2,7 @@ package com.Interview.InterviewTasksMuhtar.Numbers;
 
 public class CountDigits_Of_A_Number {
     public static void main(String[] args) {
-        int num = 12345;
+        int num = -569885;
         System.out.println(countDigits_of_Numbers1(num));
         System.out.println(countDigits_of_Numbers2(num));
     }
@@ -12,7 +12,7 @@ public class CountDigits_Of_A_Number {
     static int countDigits_of_Numbers1(int num){
         int count=0;
 
-        while(num>0){
+        while(num != 0){
             num = num/10;
             count++;
         }
@@ -21,8 +21,13 @@ public class CountDigits_Of_A_Number {
 
     //Solution 2
     static int countDigits_of_Numbers2(int num){
+        int count;
+        if(num>0)
+            count = String.valueOf(num).length();
+        else
+            count = String.valueOf(num).length() - 1;// "-" also counted as a character in string
 
-       int count = String.valueOf(num).length();
+       //int count = String.valueOf(num).length();
         return count;
     }
 }
