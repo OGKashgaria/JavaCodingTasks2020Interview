b56b7f2965fe0c3edf4edee8355b396455055d7a
OGKashgaria <oghlan.ghulam.kashgaria@gmail.com>
2020-07-09 22:51:43 -0500

    PassWord Validation with Gherkin Language was updated

--- src/com/Interview/InterviewTasksMuhtar/Array/FindDupsFromArray.java
+++ src/com/Interview/InterviewTasksMuhtar/Array/FindDupsFromArray.java
@@ -3,19 +3,28 @@ package com.Interview.InterviewTasksMuhtar.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 
 public class FindDupsFromArray {
+    static ArrayList<String> dupList;
     public static void main(String[] args) {
-        String[] arr = {"java", "Selenium","abc","Cucumber","Cucumber","cucumber","java","abc"};
-        System.out.println(checkExistenceOfDupElementsOfArray3(arr));
+        //String[] arr = {"java", "Selenium","abc","Cucumber","Cucumber","cucumber","java","abc"};
+        String[] arr = {"java", "Selenium","abc","cucumber","java","abc"};
+        //System.out.println(checkExistenceOfDupElementsOfArray1(arr));
+
+        dupList = checkExistenceOfDupElementsOfArray4(arr);
+        for (String each : dupList)
+            System.out.println(each);
+
     }
 
     //Solution 1
-    static boolean checkExistenceOfDupElementsOfArray(String[] arr) {
+    static boolean checkExistenceOfDupElementsOfArray1(String[] arr) {
         boolean flag = false;
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr.length; j++) {
-                if (arr[i] == arr[j])
+        int i =0;
+        for ( i = 0; i < arr.length; i++) {
+            for (i= i+1; i < arr.length; i++) {
+                if (arr[i] == arr[i])
                     flag=true;
             }
         }
@@ -52,5 +61,24 @@ public class FindDupsFromArray {
         }
 
         return dupArr;
+    }
+
+    static ArrayList<String> checkExistenceOfDupElementsOfArray4(String[] arr) {
+
+            dupList = new ArrayList<>();
+        //String dupArr = "";
+        LinkedHashSet<String> setDups = new LinkedHashSet<>();
+
+
+        for (String each : arr) {
+            setDups.add(each);
+            System.out.println(setDups.add(each));
+//            if (setDups.add(each)==false){
+//                dupList.add(each);
+//            }
+        }
+        System.out.println(setDups);
+
+        return dupList;
     }
 }
